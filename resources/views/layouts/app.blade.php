2561b28301335944724f7d72e078809a203b9bff
Daniel Soloway <dansoloway@gmail.com>
2025-11-02 11:02:28 +0200

    Add Font Awesome icons to all layouts
    - Add Font Awesome 6.4.0 CDN to app and admin layouts
    - Fixes missing play button icons throughout the application

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
     <link rel="manifest" href="{{ asset('site.webmanifest') }}">
     
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    
     <link rel="stylesheet" href="{{ asset('css/app.css') }}?v={{ time() }}">
     @stack('styles')
 </head>
